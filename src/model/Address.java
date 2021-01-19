5fdc9ddd3771b4949e3775bf4e19d2a770c424c1
Prabhat Gyawali <gyawaliprabhat234@gmail.com>
2021-01-19 11:27:33 -0600

    repository create

---
+++ src/model/Address.java
@@ -0,0 +1,65 @@
+package model;
+
+import java.util.Locale;
+
+
+public class Address {
+	
+	private final String id;
+	
+	private String street;
+	
+	private String city;
+	
+	private String state;
+	
+	private String zip;
+
+	public Address() {
+		this.id = "";
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [street=" + street + ", city=" + city + ", state=" + state + ", zip=" + zip + "]";
+	}
+	
+	
+
+}
---
+++ src/model/Airport.java
@@ -0,0 +1,55 @@
+package model;
+
+import java.util.UUID;
+
+public class Airport {
+	
+	private final String id;
+	
+	private String code;
+	
+	private String name;
+	
+	private Address address;
+
+	public Airport(String code, String name, Address address) {
+		this.id = "";
+		this.code = code;
+		this.name = name;
+		this.address = address;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Airport [id=" + id + ", code=" + code + ", name=" + name + ", Address=" + address + "]";
+	}
+
+}
